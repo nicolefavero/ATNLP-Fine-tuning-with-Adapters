f2be2dfc03612e67ef4e67e9fcb04de45b21ca44
Magnus <97634880+MagnusS0@users.noreply.github.com>
2024-11-24 15:16:49 +0100

    feat: add Transformer

--- transformer.py
+++ transformer.py
@@ -123,7 +123,7 @@ class Encoder(nn.Module):
 	self.dropout = nn.Dropout(dropout)
 	self.max_len = max_len
 
-	pos_weight = get_sinusoid_table(max_len + 1, emb_dim)
+	pos_weight = get_sinusoid_table(max_len + 1, emb_dim).to(device=x.device)
 	self.tok_emb = nn.Embedding(vocab_size, emb_dim, _freeze=True)
 	self.pos_emb = nn.Embedding(max_len, emb_dim, _freeze=True).from_pretrained(pos_weight)
 	
@@ -133,7 +133,7 @@ class Encoder(nn.Module):
 
     def forward(self, x, mask):
 	seq_len = x.size(1)
-	tok_emb = self.tok_emb(x)
+	tok_emb = self.tok_emb(x).to(device=x.device)
 	pos_emb = self.pos_emb(torch.arange(1, seq_len + 1, device=x.device))
 	embedding = tok_emb + pos_emb.unsqueeze(0)
 	embedding = self.dropout(embedding)
@@ -207,3 +207,91 @@ class Decoder(nn.Module):
         output = self.out_layer(x)
 
         return output
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        src_vocab_size,
+        tgt_vocab_size,
+        src_pad_idx,
+        tgt_pad_idx,
+        emb_dim=512,
+        num_layers=6,
+        num_heads=8,
+        forward_dim=2048,
+        dropout=0.0,
+        max_len=128,
+    ):
+        super().__init__()
+
+        self.encoder = Encoder(
+		src_vocab_size,
+	        emb_dim,
+	        num_layers,
+	        num_heads,
+	        forward_dim,
+	        dropout,
+	        max_len,
+	)
+	self.decoder= Decoder(
+		tgt_vocab_size,
+	        emb_dim,
+	        num_layers,
+	        num_heads,
+	        forward_dim,
+	        dropout,
+	        max_len,
+	)
+
+	self.src_pad_idx = src_pad_idx
+	self.tgt_pad_idx = tgt_pad_idx
+    def create_src_mask(self, src):
+        device = src.device
+        # (batch_size, 1, 1, src_seq_len)
+        src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+        return src_mask.to(device)
+
+    def create_tgt_mask(self, tgt):
+        device = tgt.device
+        batch_size, tgt_len = tgt.shape
+        tgt_mask = (tgt != self.tgt_pad_idx).unsqueeze(1).unsqueeze(2)
+        tgt_mask = tgt_mask * torch.tril(torch.ones((tgt_len, tgt_len))).expand(
+            batch_size, 1, tgt_len, tgt_len
+        ).to(device)
+        return tgt_mask
+
+    def forward(self, src, tgt):
+        src_mask = self.create_src_mask(src)
+	tgt_mask = self.create_tgt_mask(tgt)
+
+	encode_out = self.encoder(src, src_mask)
+	decode_out = self.decoder(tgt, encode_out, src_mask, tgt_mask)
+
+        return decode_out
+
+def main():
+	device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+	model = Transformer(
+	    src_vocab_size=200,
+	    tgt_vocab_size=220,
+	    src_pad_idx=0,
+	    tgt_pad_idx=0,
+	).to(device)
+	
+	# source input: batch size 4, sequence length of 75
+	src_in = torch.randint(0, 200, (4, 75)).to(device)
+	
+	# target input: batch size 4, sequence length of 80
+	tgt_in = torch.randint(0, 220, (4, 80)).to(device)
+	
+	# expected output shape of the model
+	expected_out_shape = torch.Size([4, 80, 220])
+	
+	with torch.no_grad():
+	    out = model(src_in, tgt_in)
+	
+	assert out.shape == expected_out_shape, f"wrong output shape, expected: {expected_out_shape}"
+
+if __name__ == "__main__":
+	main()
